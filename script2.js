41b623035636e4fdb19b2c6f5ff23e67d56a5647
NamanRajoriaCACsem3 <150421147+NamanRajoriaCACsem3@users.noreply.github.com>
2023-11-10 13:46:26 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,55 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
+
+// Smooth scrolling for all links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
+
+// Slide animation
+const left = document.getElementById("left-side");
+
+const handleMove = e => {
+  left.style.width = `${e.clientX / window.innerWidth * 100}%`;
+}
+
+document.onmousemove = e => handleMove(e);
+
+document.ontouchmove = e => handleMove(e.touches[0]);
+
+
+// Add this JavaScript code to script2.js
+const globalizationVideo = document.getElementById("globalization-video-source");
+const videoContainer = document.getElementById("globalization-video");
+
+videoContainer.addEventListener("mouseover", function () {
+    // Play the video when the cursor is over the video container
+    globalizationVideo.play();
+});
+
+videoContainer.addEventListener("mouseout", function () {
+    // Pause the video when the cursor leaves the video container
+    globalizationVideo.pause();
+});
+
